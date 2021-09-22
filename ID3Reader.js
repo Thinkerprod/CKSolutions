6d81c5dd64217412c7d9af71a54bc8947d585953
Cory Kutschker <cory.kutschker@gmail.com>
2021-09-22 16:38:42 -0600

    git add

---
+++ ID3Reader.js
@@ -0,0 +1,24 @@
+document.querySelector('input[type="file"]').onchange=function(e){
+
+     
+    var audReader= new FileReader();
+
+
+
+        audReader.onload=function (e){
+        var dv=new jDataView(this.result);
+        
+
+        if(dv.getString(3, dv.byteLength-128)){
+            var title=dv.getString(30, dv.tell());
+            var artist=dv.getString(30, dv.tell());
+            var album=dv.getString(30, dv.tell());
+            var year=dv.getString(4, dv.tell());
+        }
+        else{
+
+        }
+        };
+
+        audReader.readAsArrayBuffer(this.files[0]);
+    };
