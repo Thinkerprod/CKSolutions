9674d1d6087ba63bda4850f2fa0de2a47f6646a2
Cory <23246312+Thinkerprod@users.noreply.github.com>
2022-03-07 21:51:03 -0600

    added picture reader

--- js/ID3Reader.js
+++ js/ID3Reader.js
@@ -1,10 +1,24 @@
 var jsmediatags = require("jsmediatags");
+exports.songReader=function(){
 jsmediatags.read("../Music/Pink_Floyd/WishYouWereHere/ShineOnYouCrazyDiamond(PartsI-V).mp3", {
   onSuccess: function(tag) {
-    console.log(tag);
+    // console.log(tag);
+    
+    var tags=tag.tags;
+    return tags;
   },
   onError: function(error) {
     console.log(':(', error.type, error.info);
   }
 });
+}
 
+function albumDisplay(id, picture){
+    const { data, format } = picture;
+let base64String = "";
+for (const i = 0; i < data.length; i++) {
+  base64String += String.fromCharCode(data[i]);
+}
+
+document.getElementById(id).src = `data:${data.format};base64,${window.btoa(base64String)}`;
+}
