b79d396a9ecd4f40b68ab6134f36f035c96415ce
Cory <23246312+Thinkerprod@users.noreply.github.com>
2022-03-12 16:30:06 -0600

    whatever

--- Music_App/js/ID3Reader.js
+++ Music_App/js/ID3Reader.js
@@ -1,13 +1,31 @@
 
 var jsmediatags = require('jsmediatags');
-var express= require('express')
-
+var express= require('express');
+var app=express();
+var tagsArray=new Array();
 jsmediatags.read("../Music/Pink_Floyd/WishYouWereHere/ShineOnYouCrazyDiamond(PartsI-V).mp3", {
   onSuccess: function(tag) {
     // console.log(tag);
     
     var tags=tag.tags;
-    return tags;
+    tagsArray=[tags.artist,tags.track,tags.album,tags.title,tags.picture];
+
+
+    var http = require('http');
+
+var data = "data to send to client";
+
+var server = http.createServer(function (request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(data); // You Can Call Response.write Infinite Times BEFORE response.end
+    response.end("Hello World\n");
+}).listen(5500);
+    // var artist=tags.artist;
+    // app.get('/ID3Reader.js', function (req, res) {
+    //   res.json({Array:tagsArray});
+    // })
+    
+    // app.listen(5500)
   },
   onError: function(error) {
     console.log(':(', error.type, error.info);
@@ -25,3 +43,6 @@ for (const i = 0; i < data.length; i++) {
 var pictureData = `data:${data.format};base64,${window.btoa(base64String)}`;
 return pictureData;
 }
+
+
+
