5c7ab74fc895582ec9ba595ecbda9e2c99f0a5cc
Cory <23246312+Thinkerprod@users.noreply.github.com>
2022-04-02 14:15:55 -0600

    added pug file and server for pug

--- Music_App/js/ID3Reader.js
+++ Music_App/js/ID3Reader.js
@@ -6,6 +6,22 @@ var app=express();
 var pug=require('pug');
 var tagsArray=Array();
 
+const port=3000;
+const host='localhost';
+
+app.set('views','./views');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res)=>{
+
+  res.render('index');
+
+});
+
+app.listen(port, host, ()=>{
+
+  console.log("server started at "+host+"port:"+port);
+})
 
 
 
@@ -21,12 +37,12 @@ jsmediatags.read("../Music/Pink_Floyd/WishYouWereHere/ShineOnYouCrazyDiamond(Par
 
 var data = "data to send to client";
 
-var server = http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write(data); // You Can Call Response.write Infinite Times BEFORE response.end
-    response.end("Hello World\n");
+// var server = http.createServer(function (request, response) {
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write(data); // You Can Call Response.write Infinite Times BEFORE response.end
+//     response.end("connected");
   
-}).listen(5500);
+// }).listen(5500);
     // var artist=tags.artist;
     // app.get('/ID3Reader.js', function (req, res) {
     //   res.json({Array:tagsArray});
@@ -40,7 +56,7 @@ var server = http.createServer(function (request, response) {
 });
 
 
-function albumDisplay(id, picture){
+function albumDisplay(picture){
     const { data, format } = picture;
 let base64String = "";
 for (const i = 0; i < data.length; i++) {
