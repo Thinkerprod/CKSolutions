709d4ba21f2aaba9c172785a6fd4776d7841b9b4
Cory <23246312+Thinkerprod@users.noreply.github.com>
2022-05-15 21:20:45 -0600

    progress bar now works

--- Music_App/public/js/music-controls.js
+++ Music_App/public/js/music-controls.js
@@ -1,17 +1,25 @@
 
 console.log("js loaded")
+var songDuration="-:--"
+var songElapsed="-:--"
+var globalBarWidth=0
+var myReq=""
+var progBarWidthRate=""
 
-    document.getElementById("songDur").innerText = document.getElementById("player").duration
+    document.getElementById("elapse").innerText= songElapsed
+    document.getElementById("songDur").innerText = songDuration
+    
     
     document.getElementById('playBtn').addEventListener('click',function (e){
-        
+        // progBarWidthRate = (100/document.getElementById("player").duration)
         var song=document.getElementById('player')
         song.play();
         var min=Math.floor(document.getElementById("player").duration/60)
         var sec=Math.floor(document.getElementById("player").duration%60)
         document.getElementById("songDur").innerText = min+":"+sec
+        myReq=window.requestAnimationFrame(timeProgress())
+                
 
-        
 
     })
     document.getElementById('stopBtn').addEventListener('click',function (e){
@@ -19,6 +27,9 @@ console.log("js loaded")
         const song=document.getElementById('player')
         song.pause()
         song.currentTime=0
+        window.cancelAnimationFrame(myReq)
+        globalBarWidth=0
+
 
     })    
     document.getElementById('pauseBtn').addEventListener('click',function (e){
@@ -39,7 +50,22 @@ console.log("js loaded")
     
 function timeProgress(){
 
+//Get elapsed time incrementing and display it
+var elapsed=document.getElementById("player").currentTime
+var minElapse=Math.floor(elapsed/60)
+var secElapse=Math.floor(elapsed%60)
+secElapse=('0'+secElapse).slice(-2)
+document.getElementById("elapse").innerText=minElapse+":"+secElapse
 
+var progBarWidthRate = ((elapsed*100)/document.getElementById("player").duration)
+// console.log(progBarWidthRate)
+document.getElementById("progBar").style.width=progBarWidthRate+"%"
+
+
+window.requestAnimationFrame(timeProgress)
+if(document.getElementById("progBar").style.width=="100%"){
+    window.cancelAnimationFrame(myReq)
+}
 
 }
 
