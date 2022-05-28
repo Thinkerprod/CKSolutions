66803673ee0632eecbbd415898245287d77455af
Cory <23246312+Thinkerprod@users.noreply.github.com>
2022-05-28 16:18:32 -0600

    Volume controls officially work

--- Music_App/public/js/music-controls.js
+++ Music_App/public/js/music-controls.js
@@ -5,59 +5,44 @@ var songElapsed="-:--"
 var globalBarWidth=0
 var myReq=""
 var progBarWidthRate=""
+var audioElement=""
+var globalPath=""
+var globalVolume=""
+
+
+
+function createMusicPlayer(path){
+    audioElement=new Audio(path)
+    audioElement.volume=globalVolume
+    var volumeHeight=Math.floor(audioElement.volume*100)+"%"
+   
+document.getElementById("volControl").style.height=volumeHeight
+}
+
+
+setVolumeDefault(0.25)
+
+
 
     document.getElementById("elapse").innerText= songElapsed
     document.getElementById("songDur").innerText = songDuration
     
     
-    document.getElementById('playBtn').addEventListener('click',function (e){
-        // progBarWidthRate = (100/document.getElementById("player").duration)
-        var song=document.getElementById('player')
-        song.play();
-        var min=Math.floor(document.getElementById("player").duration/60)
-        var sec=Math.floor(document.getElementById("player").duration%60)
-        document.getElementById("songDur").innerText = min+":"+sec
-        myReq=window.requestAnimationFrame(timeProgress)
-                
-
-
-    })
-    document.getElementById('stopBtn').addEventListener('click',function (e){
-
-        const song=document.getElementById('player')
-        song.pause()
-        song.currentTime=0
-        window.cancelAnimationFrame(myReq)
-        globalBarWidth=0
-
-
-    })    
-    document.getElementById('pauseBtn').addEventListener('click',function (e){
-
-        const song=document.getElementById('player')
-        if(!song.paused){
-            console.log(song.paused)
-            song.pause()
-        }
-        else{
-            song.play()
-        }
-        
-        
-
-    })
+    document.getElementById('playBtn').addEventListener('click',function (e){play()})
+    document.getElementById('stopBtn').addEventListener('click',function (e){stop()})    
+    document.getElementById('pauseBtn').addEventListener('click',function (e){pause()})
 
     
 function timeProgress(){
 
 //Get elapsed time incrementing and display it
-var elapsed=document.getElementById("player").currentTime
+var elapsed=audioElement.currentTime
 var minElapse=Math.floor(elapsed/60)
 var secElapse=Math.floor(elapsed%60)
 secElapse=('0'+secElapse).slice(-2)
 document.getElementById("elapse").innerText=minElapse+":"+secElapse
 
-var progBarWidthRate = ((elapsed*100)/document.getElementById("player").duration)
+var progBarWidthRate = ((elapsed*100)/audioElement.duration)
 // console.log(progBarWidthRate)
 document.getElementById("progBar").style.width=progBarWidthRate+"%"
 
@@ -68,5 +53,92 @@ if(document.getElementById("progBar").style.width=="100%"){
 }
 
 }
+
+function play(){
+    var song=audioElement
+        song.play();
+        var min=Math.floor(audioElement.duration/60)
+        var sec=Math.floor(audioElement.duration%60)
+        document.getElementById("songDur").innerText = min+":"+sec
+        myReq=window.requestAnimationFrame(timeProgress)
+}
+
+function pause(){
+
+    var song=audioElement
+    if(!song.paused){
+        console.log(song.paused)
+        song.pause()
+    }
+    else{
+        song.play()
+    }
+
+}
+
+function stop(){
+    var song=audioElement
+        song.pause()
+        song.currentTime=0
+        window.cancelAnimationFrame(myReq)
+        globalBarWidth=0
+}
+
+function setVolumeDefault(volumeDefault){
+    
+    globalVolume=volumeDefault
+    // var volumeHeight=(audioElement.volume*100)+"%"
+    // document.getElementById("volControl").style.height=volumeHeight
+    
+}
+
+
+
+function increaseVolume(){
+    if(audioElement.volume<1){
+        var volumeLevel=globalVolume
+        volumeLevel+=0.01
+        globalVolume=volumeLevel
+    audioElement.volume=volumeLevel
+    var volumeHeight=Math.floor(volumeLevel*100)+"%"
+    
+document.getElementById("volControl").style.height=volumeHeight
+document.getElementById("volLabel").innerText=volumeHeight
+console.log(audioElement.volume)
+    }
+}
+
+function decreaseVolume(){
+if(audioElement.volume>0){
+    var volumeLevel=globalVolume
+        volumeLevel-=0.01
+        globalVolume=volumeLevel
+    audioElement.volume=volumeLevel
+    var volumeHeight=Math.floor(volumeLevel*100)+"%"
+    
+document.getElementById("volControl").style.height=volumeHeight
+document.getElementById("volLabel").innerText=volumeHeight
+console.log(audioElement.volume)
+}
+}
+
+document.getElementById("plus").addEventListener("click",function(e){
+    increaseVolume()
+})
+
+document.getElementById("minus").addEventListener("click",function(e){
+    decreaseVolume()
+})
+
+document.getElementById("mute").addEventListener("click", function (e){
+    if(audioElement.muted==false){
+        audioElement.muted=true
+    }
+    else{
+        audioElement.muted=false
+    }
+})
+
+
 
 
