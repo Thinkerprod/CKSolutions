ebe974d10359d85b38e671628bb98ce0efda068a
Cory Kutschker <23246312+Thinkerprod@users.noreply.github.com>
2022-12-28 13:32:10 -0600

    learning node.js properly

--- Notes_App/app.js
+++ Notes_App/app.js
@@ -1,5 +1,9 @@
+const chalk=require('chalk')
+
+const yargs=require('yargs')
 const fs=require('fs')
 const validator=require('validator')
+
 const getNote=require('./notes.js')
 var message=getNote();
 console.log(message);
@@ -8,7 +12,51 @@ fs.writeFileSync('notes.txt','bleh bleh bleh');
 
 fs.appendFileSync('notes.txt',' I do not say bleh bleh bleh')
 
-//1.create a new file called 'notes.js'
-//2. create getnotes function that returns "Your notes"
-//3. export getnotes function 
-//4. from app.js , load in and call the function printing message to console
+console.log(chalk.green.bold("success!"))
+// add command
+yargs.command({
+    command: 'add',
+    describe: 'add a note',
+    builder:{
+        title:{
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+            
+        }
+    },
+    handler: function(){
+        console.log("added a note")
+    }
+})
+
+//remove command
+yargs.command({
+    command:'remove',
+    describe: 'removes a note',
+    handler: function(){
+        console.log("removed the note")
+    }
+})
+
+//list command
+yargs.command({
+    command: 'list',
+    describe: 'lists the notes',
+    handler: function(){
+        console.log("listed notes :)")
+    }
+})
+
+//read command
+yargs.command({
+    command: 'read',
+    describe: 'reads the note',
+    handler: function (){
+        console.log("reading the note")
+    }
+})
+
+yargs.parse()
