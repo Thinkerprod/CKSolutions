12c88a95a76db8c946ea74954bfa693a453e319e
Cory Kutschker <23246312+Thinkerprod@users.noreply.github.com>
2023-03-27 00:26:44 -0600

    made both blog banners

--- CKSite/business/includes/intro-section.php
+++ CKSite/business/includes/intro-section.php
@@ -3,10 +3,10 @@
 <div class="intro-area">
 
   <div class="introWrapper">
-    <h1 class="intro"><span class="hello">Hello,</span> <br> My name is</br>John Smith. <br> I'm a <br>web developer.</h1>
+    <h1 class="intro"><span class="hello">Hello,</span> <br> My name is</br>Cory Kutschker. <br> I'm a <br>web developer.</h1>
   </div>
   <div class="hire">
-    <img src="https://placekitten.com/75/75" alt="" class="head">
+    <img src="images/Cory-Profile.jpg" alt="" class="head">
     <div class="hireCombo">
       <div class="blackCircle"></div>
       <div class="hireBtn">
@@ -22,10 +22,10 @@
 
   <div class="introWrapper">
     <div class="mask"></div>
-    <h1 class="intro introFlipped"><span class="hello">Hello,</span> <br><span class="flipText">My name is</span> </br><span class="flipText">John Smith.</span> <br> <span class="flipText">I'm a</span> <br><span class="flipText">web developer.</span></h1>
+    <h1 class="intro introFlipped"><span class="hello">Hello,</span> <br><span class="flipText">My name is</span> </br><span class="flipText">Cory Kutschker.</span> <br> <span class="flipText">I'm a</span> <br><span class="flipText">web developer.</span></h1>
   </div>
   <div class="hire">
-    <div class="head rotated"><img src="https://placekitten.com/100/100" alt="" class="profile"></div>
+    <div class="head rotated"><img src="images/Cory-Profile.jpg" alt="" class="profile"></div>
     <div class="hireCombo">
       <div class="blackCircle"></div>
       <div class="hireBtn introFlipped hireBtnFlipped">
