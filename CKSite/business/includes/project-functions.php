b4cbd61096ca2308eb485b3db63f15378ea0037b
Cory Kutschker <23246312+Thinkerprod@users.noreply.github.com>
2023-11-07 21:16:05 -0600

    trying to fix the bootstrap in project section

--- CKSite/business/includes/project-functions.php
+++ CKSite/business/includes/project-functions.php
@@ -13,14 +13,16 @@ $proj_desc=$row['proj_desc'];
 
 
 echo $proj_name;
-$currentProject="<div class='col'>
+$currentProject="
 <div class='card'>
 <img src='images/projects/".$proj_image."' alt='".$proj_desc."' class='img-project card-img-top'>
-<div class='card-body'>
+<div class='card card-body'>
 <h5 class='card-title'>".$proj_name."</h5>
-<h6 class='project-stack'>".$proj_stack."</h6>
+<h6 class='card-subtitle mb-2 text-body-secondary'>".$proj_stack."</h6>
 <p class='card-text'>".$proj_desc."</p>
-<a href='".$proj_location."' class='card-link'>More Info</a>
+<div class='card-footer'><a href='".$proj_location."' class='card-link'>More Info</a>
+</div>
+
 </div>
 </div>
 </div>";
--- CKSite/business/includes/project-section.php
+++ CKSite/business/includes/project-section.php
@@ -2,18 +2,20 @@
 <?php include_once("../php_util/db.php"); ?>
 
 
-<section id="project-section" class="projSection">
-<h2 class="projHeader headers">Portfolio</h2>
+<section id="project-section" class="projSection container-fluid">
+<h2 class="Display-2">Portfolio</h2>
+
+
 <button class="btn btn-light" type="button" data-bs-toggle="collapse" data-bs-target="#CurrentProjects" aria-expanded="false" aria-controls="CurrentProjects">Current Projects</button>
 
 
 <!-- Completed projects content -->
 
 
-    <div class="collapse multi-collapse" id="CurrentProjects">
-      <div class="container-fluid">
+    <div class="container collapse multi-collapse" id="CurrentProjects">
+      
     <div class="row">
-  <div class="col col-md-3">
+  <div class="col-sm-1 col-md-2 col-lg-3">
       <?php getAllCurrentProjects($connection); ?>
     </div>
   </div>
@@ -24,10 +26,10 @@
 
 <button class="btn btn-light" type="button" data-bs-toggle="collapse" data-bs-target="#PastProjects" aria-expanded="false" aria-controls="PastProjects">Past Projects</button>
 
-    <div class="collapse multi-collapse" id="PastProjects">
-    <div class="container-fluid">
+    <div class=" container collapse multi-collapse" id="PastProjects">
+   
 <div class="row">
-  <div class="col col-md-3">
+  <div class="col-sm-1 col-md-2 col-lg-3">
     <?php getAllPastProjects($connection); ?>
     </div>
   </div>
